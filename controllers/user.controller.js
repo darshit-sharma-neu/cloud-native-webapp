85b621b9539e74abdf929b279d94d2744a4d5ea9
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-10 03:50:16 -0400

    fail test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,7 +21,7 @@ async function postController(req, res, next) {
             inputFields.length == 0 ||
             !inputFields.every((field) => requiredFields.includes(field))
         ) {
-            res.status(400).send();
+            res.status(401).send();
             return;
         }
         if(!req.body.password){

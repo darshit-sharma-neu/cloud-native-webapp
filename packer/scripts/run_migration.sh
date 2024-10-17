7bb26aa3935f280da680094e1c3946b9b91f3cc3
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-17 01:37:54 -0400

    packer build v15

--- packer/scripts/run_migration.sh
+++ packer/scripts/run_migration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Running migration"
-chown -R csye6225:csye6225 /home/webapp
+sudo chown -R csye6225:csye6225 /home/webapp
 sudo /usr/bin/node /home/webapp/scripts/database/createDatabase.js

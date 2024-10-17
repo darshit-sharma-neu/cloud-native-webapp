e745492ac8321eb836e2acdd642967d93eea99f9
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-16 21:36:22 -0400

    fix: build workflow fix 1

--- packer/scripts/install_node.sh
+++ packer/scripts/install_node.sh
@@ -23,5 +23,5 @@ sudo chown -R ubuntu:ubuntu /opt/webapp
 # Install dependencies
 cd /opt/webapp
 echo "Installing dependencies"
-sudo npm install
+npm install
 echo "Webapp setup completed"
--- packer/scripts/run_migration.sh
+++ packer/scripts/run_migration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running migration"
-sudo /home/ubuntu/.nvm/versions/node/v20.17.0/bin/node /opt/webapp/scripts/database/createDatabase.js
+/home/ubuntu/.nvm/versions/node/v20.17.0/bin/node /opt/webapp/scripts/database/createDatabase.js

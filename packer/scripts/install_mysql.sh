ee3603b0305b1cc56b7b4cba98fa61aebed1b990
datshit-sharma-neu <sharma.dar@northeastern.edu>
2024-10-16 23:45:11 -0400

    packer build v4

--- packer/scripts/install_mysql.sh
+++ packer/scripts/install_mysql.sh
@@ -8,7 +8,7 @@ sudo systemctl start mysql.service
 
 # Create new user
 echo "Creating db user"
-sudo mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';FLUSH PRIVILEGES;"
+sudo mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';FLUSH PRIVILEGES;CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 # sudo mysql -u root -p$DB_PASSWORD -e "FLUSH PRIVILEGES;"
 # # Grant privileges to the new user
 # sudo mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost';"
